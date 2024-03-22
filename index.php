004c60ff1aef3fed1e915115e5bcd0a99382c0f6
Richardo Teddy <90501677+RichardoTeddy@users.noreply.github.com>
2024-03-22 11:44:30 +0700

    update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require 'cek.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Dashboard Utama ajah</title>
+    <title>Dashboard Utama </title>
     <link href="css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
